b6bc8b4f7a647ef799c4a98ab8623b32feb901c6
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-11-30 20:51:55 +0700

    adding file for socials

---
+++ src/collections/Platforms/PlatformIconLink.tsx
@@ -0,0 +1,9 @@
+import type { Platform } from '@/payload-types'
+
+import React from 'react'
+
+const PlatformIconLink = () => {
+  return <div>PlatformIconLink</div>
+}
+
+export default PlatformIconLink
