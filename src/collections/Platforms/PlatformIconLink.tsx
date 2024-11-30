9b3b3794212f6c55dcd7b6e1cfb3c3da43457508
Batman <abheysehrawat@gmail.com>
2024-12-01 00:51:17 +0530

    Resolved PlatformIconLink image tags

--- src/collections/Platforms/PlatformIconLink.tsx
+++ src/collections/Platforms/PlatformIconLink.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Platform } from '@/payload-types';
+import Image from 'next/image';
 
-interface PlatformIconLinkProps {
-  platform: Platform;
-}
 
-const PlatformIconLink: React.FC<PlatformIconLinkProps> = ({ platform }) => {
-  if (!platform) return null;
-
-  const { name, link, image } = platform;
+const PlatformIconLink: React.FC<any> = ({image,link}) => {
 
   const imageUrl = image || '/default-icon.png'; 
 
@@ -19,11 +14,11 @@ const PlatformIconLink: React.FC<PlatformIconLinkProps> = ({ platform }) => {
       rel="noopener noreferrer"
       className="flex items-center justify-center w-16 h-16 bg-gray-100 rounded-full hover:bg-gray-200 transition"
     >
-      <img
-        src={imageUrl} 
-        alt={name}
-        className="w-12 h-12 object-cover rounded-full"
-      />
+     <div className="relative h-[40vh] sm:h-[40vh] md:h-[60vh] lg:min-h-[50vh] w-full lg:w-[45%]">
+        {typeof image !== 'string' && image?.url && (
+          <Image src={image.url} alt={image.alt} fill className="object-contain" />
+        )}
+      </div>
     </a>
   );
 };
