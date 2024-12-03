e1efa0b45cf35d320da97e8bb8a5a60fe9c786ae
Batman <abheysehrawat@gmail.com>
2024-12-04 00:41:23 +0530

    social section updates #44

--- src/components/ui/headingSection.tsx
+++ src/components/ui/headingSection.tsx
@@ -7,8 +7,8 @@ interface TextSectionProps {
 
 const TextSection: React.FC<TextSectionProps> = ({ heading, paragraph }) => {
   return (
-    <div className="w-full text-center default-x-padding pt-5">     
-      <p className="text-paragraph-primary">{paragraph}</p>
+    <div className="w-full text-center default-x-padding py-3">     
+      <p className="text-paragraph-primary pb-3">{paragraph}</p>
       <h2 className="text-subheading">{heading}</h2>
     </div>
   );
