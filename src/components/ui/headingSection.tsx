3922d2bba1e16dccdd9b886ff7db1e23271dee23
Elias Heininger <eliasheininger@web.de>
2024-12-03 19:45:06 +0700

    changes to social section, heading component and Hero text (#40)
    Co-authored-by: Elias Heininger <eliasheininger@Eliass-MacBook-Pro.local>

--- src/components/ui/headingSection.tsx
+++ src/components/ui/headingSection.tsx
@@ -7,9 +7,9 @@ interface TextSectionProps {
 
 const TextSection: React.FC<TextSectionProps> = ({ heading, paragraph }) => {
   return (
-    <div className="w-full text-center default-x-padding pt-5">
-      <h2 className="text-heading">{heading}</h2>
+    <div className="w-full text-center default-x-padding pt-5">     
       <p className="text-paragraph-primary">{paragraph}</p>
+      <h2 className="text-subheading">{heading}</h2>
     </div>
   );
 };
