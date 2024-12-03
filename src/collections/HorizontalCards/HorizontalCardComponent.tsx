2e5f4de6038b27dddd3de8b4973e5daf1dc5507d
Akshat Mittal <93286254+drifter089@users.noreply.github.com>
2024-12-03 22:33:37 +0700

    fixing border radius and padding (#42)

--- src/collections/HorizontalCards/HorizontalCardComponent.tsx
+++ src/collections/HorizontalCards/HorizontalCardComponent.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 
 const HorizantalCard: React.FC<Horizontalcard> = ({ heading, content, image }) => {
   return (
-    <div className="w-80 m-h-[44rem] md:w-auto max-w-3xl rounded-xl border shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-wrap md:flex-nowrap">
+    <div className="w-[20rem] md:w-auto max-w-3xl rounded-[2.5rem] overflow-hidden border shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-wrap md:flex-nowrap">
       <div className="relative h-[20rem] w-[20rem]">
         {typeof image !== 'string' && image?.url && (
-          <Image src={image.url} alt={image.alt} fill className="object-contain" />
+          <Image src={image.url} alt={image.alt} fill className="object-cover" />
         )}
       </div>
 
-      <div className="w-[30rem] md:w-[60%] flex flex-col justify-center">
+      <div className="md:w-[60%] p-3 flex flex-col justify-center gap-2">
         <h2 className="text-title-bold">{heading}</h2>
         <p className="text-paragraph">{content}</p>
       </div>
