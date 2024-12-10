c0d909ee7a39348ee3fb4aa6239483c867089280
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-12-11 05:02:03 +0700

    adding meethub page so it naviages to it from events page

--- src/globals/NavBar/NavBarComponent.tsx
+++ src/globals/NavBar/NavBarComponent.tsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 
 export default function NavBarComponent({ name, navigationLink }: { name: string | null | undefined; navigationLink: string }) {
     // console.log('blocks', prop);
-    return ( <NavigationMenuItem>
-        <Link href= {`#${navigationLink}`} legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {name}
-          </NavigationMenuLink>
+    return (
+      <NavigationMenuItem>
+        <Link href={`/meethub#${navigationLink}`} legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>{name}</NavigationMenuLink>
         </Link>
       </NavigationMenuItem>
-    );
+    )
 }
