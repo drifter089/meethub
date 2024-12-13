24e3cf48b1b6966846b229328acd4e8207667692
Batman2809 <32994755+Batman2809@users.noreply.github.com>
2024-12-13 23:58:01 +0530

    Git issue 53: Space between heading and subheading in socials section. (#59)
    * social section updates #44
    
    * Added navigation bar
    
    * Updated Nav bar as per review comments
    
    * Reverted global.css
    
    * Resolved PR comments
    
    * GIT issue:53 Updated socials section css

--- src/components/ui/headingSection.tsx
+++ src/components/ui/headingSection.tsx
@@ -11,7 +11,7 @@ const TextSection: React.FC<TextSectionProps> = ({ heading, paragraph, backgroun
     <div
       className={`${backgroundColor === 'secondary' ? 'bg-secondary' : 'bg-background'} w-full text-center default-x-padding pt-6 pb-3`}
     >
-      <p className="text-paragraph-primary pb-3">{paragraph}</p>
+      <p className="text-paragraph-primary">{paragraph}</p>
       <h2 className="text-subheading">{heading}</h2>
     </div>
   )
