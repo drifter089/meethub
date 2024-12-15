2f09254ea96015eaf76703d85880817f9a25ee59
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-12-16 02:06:40 +0700

    adding form builder frontend parts

--- src/app/(meethub)/meethub/page.tsx
+++ src/app/(meethub)/meethub/page.tsx
@@ -5,38 +5,50 @@ import LandingHeroComp from '@/blocks/Hero/Hero'
 import Socials from '@/blocks/Socials/Socials'
 import HorizontalCard from '@/collections/HorizontalCards/HorizontalCardComponent'
 import Section from '@/blocks/Section/Section'
+import { FormBlock } from '@/blocks/Form/Component'
 const page = async () => {
   const payload = await getPayload({ config: configPromise })
 
   const landingPage = await payload.findGlobal({
     slug: 'home',
   })
-  console.log("layout", landingPage);
+
+  const eventpages = await payload.find({
+    collection: 'posts',
+    limit: 1000,
+  })
+
+  console.log('layout', eventpages.docs)
+
   return (
     <>
       {landingPage.layout &&
         landingPage.layout.length > 0 &&
         landingPage.layout.map((block) => {
-          const sectionId = block.blockName ? block.blockName : block.id;
+          const sectionId = block.blockName ? block.blockName : block.id
           if (block.blockType === 'Hero') {
             return (
-                <LandingHeroComp
-                  blockType="Hero"
-                  backgroundColor={block.backgroundColor || 'primary'}
-                  heading={block.heading}
-                  content={block.content}
-                  image={block.image}
-                  reverse={block.reverse}
-                  key={block.id}
-                  blockName={sectionId}
-                />
+              <LandingHeroComp
+                blockType="Hero"
+                backgroundColor={block.backgroundColor || 'primary'}
+                heading={block.heading}
+                content={block.content}
+                image={block.image}
+                reverse={block.reverse}
+                key={block.id}
+                blockName={sectionId}
+              />
             )
           } else if (block.blockType === 'Social') {
-            return(
-              <Socials key={block.id} backgroundColor={block.backgroundColor || 'primary'} blockName={sectionId} />
+            return (
+              <Socials
+                key={block.id}
+                backgroundColor={block.backgroundColor || 'primary'}
+                blockName={sectionId}
+              />
             )
           } else if (block.blockType === 'section') {
-            return(
+            return (
               <Section
                 key={block.id}
                 backgroundColor={block.backgroundColor}
@@ -49,6 +61,18 @@ const page = async () => {
           }
           return null
         })}
+      {eventpages.docs.map((event) => {
+        if (event.form && typeof event.form !== 'string') {
+          return (
+            <FormBlock
+              key={event.id}
+              form={event.form}
+              // enableIntro={event.enableIntro}
+              // introContent={event.introContent}
+            />
+          )
+        }
+      })}
     </>
   )
 }
